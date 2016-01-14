9579db1b157d9b8e98f9fff06a8c21ae60142258
Matthew Flatt <mflatt@racket-lang.org>
2016-01-13 19:44:43 -0700

    update copyright to 2016

--- distro-build-server/readme.rkt
+++ distro-build-server/readme.rkt
@@ -77,7 +77,7 @@
       -------
      
       Racket
-      Copyright (c) 2010-2015 PLT Design Inc.
+      Copyright (c) 2010-2016 PLT Design Inc.
      
       Racket is distributed under the GNU Lesser General Public License
       (LGPL).  This means that you can link Racket into proprietary
